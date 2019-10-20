4c8135adcf5acc39e94f0c51591b07049563fc3b
Sofía Pérez <sofia.perez.salvo@gmail.com>
2019-10-20 15:56:29 +0200

    Testing coroutines through behavior

--- kotlin-coroutines-start/app/src/androidTest/java/com/example/android/kotlincoroutines/main/MainViewModelTest.kt
+++ kotlin-coroutines-start/app/src/androidTest/java/com/example/android/kotlincoroutines/main/MainViewModelTest.kt
@@ -17,14 +17,23 @@
 package com.example.android.kotlincoroutines.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.example.android.kotlincoroutines.test.util.captureValues
-import kotlinx.coroutines.runBlocking
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.google.common.truth.Truth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
+@ExperimentalCoroutinesApi
 @RunWith(JUnit4::class)
 class MainViewModelTest {
 
@@ -34,6 +43,11 @@ class MainViewModelTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    /**
+     * This dispatcher will let us progress time in the test.
+     */
+    var testDispatcher = TestCoroutineDispatcher()
+
     lateinit var subject: MainViewModel
 
     /**
@@ -41,16 +55,45 @@ class MainViewModelTest {
      */
     @Before
     fun setup() {
+        // set Dispatchers.Main, this allows our test to run
+        // off-device
+        Dispatchers.setMain(testDispatcher)
         subject = MainViewModel()
     }
 
+    @After
+    fun teardown() {
+        // reset main after the test is done
+        Dispatchers.resetMain()
+        // call this to ensure TestCoroutineDispatcher doesn't
+        // accidentally carry state to the next test
+        testDispatcher.cleanupTestCoroutines()
+    }
+
+    // note the use of runBlockingTest instead of runBlocking
+    // this gives the test the ability to control time.
     @Test
-    fun whenMainViewModelClicked_showSnackbar() {
-        runBlocking {
-            subject.snackbar.captureValues {
-                subject.onMainViewClicked()
-                assertSendsValues(2_000, "Hello, from threads!")
-            }
+    fun whenMainViewModelClicked_showSnackbar() = testDispatcher.runBlockingTest {
+        subject.snackbar.observeForTesting {
+            subject.onMainViewClicked()
+            // progress time by one second
+            advanceTimeBy(1_000)
+            // value is available immediately without making the test wait
+            Truth.assertThat(subject.snackbar.value)
+                    .isEqualTo("Hello, from coroutines!")
+        }
+    }
+
+    // helper method to allow us to get the value from a LiveData
+    // LiveData won't publish a result until there is at least one observer
+    private fun <T> LiveData<T>.observeForTesting(
+            block: () -> Unit) {
+        val observer = Observer<T> { Unit }
+        try {
+            observeForever(observer)
+            block()
+        } finally {
+            removeObserver(observer)
         }
     }
 }
