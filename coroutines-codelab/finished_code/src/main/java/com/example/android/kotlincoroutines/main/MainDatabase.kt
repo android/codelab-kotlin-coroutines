798fc9c7f28f78a17982b607c641748ad4460079
Sean McQuillan <seanmcq@google.com>
2019-12-01 12:22:30 -0800

    Star imports

--- coroutines-codelab/finished_code/src/main/java/com/example/android/kotlincoroutines/main/MainDatabase.kt
+++ coroutines-codelab/finished_code/src/main/java/com/example/android/kotlincoroutines/main/MainDatabase.kt
@@ -18,7 +18,15 @@ package com.example.android.kotlincoroutines.main
 
 import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 /**
  * Title represents the title fetched from the network
