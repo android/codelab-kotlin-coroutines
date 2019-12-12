684d33fc3e62901834def3f6442f3e217ff04d7c
Sean McQuillan <seanmcq@google.com>
2019-12-11 16:08:28 -0800

    Fix wildcard imports

--- advanced-coroutines-codelab/sunflower/src/test/java/com/example/android/advancedcoroutines/util/CacheOnSuccessTest.kt
+++ advanced-coroutines-codelab/sunflower/src/test/java/com/example/android/advancedcoroutines/util/CacheOnSuccessTest.kt
@@ -1,10 +1,14 @@
 package com.example.android.advancedcoroutines.util
 
-import com.google.common.truth.Truth
 import com.google.common.truth.Truth.assertThat
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Assert.*
+import kotlinx.coroutines.withTimeout
 import org.junit.Test
 
 class CacheOnSuccessTest {
