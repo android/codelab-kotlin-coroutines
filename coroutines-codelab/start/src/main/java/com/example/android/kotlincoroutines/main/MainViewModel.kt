cf55af361a927bb845d3d240fe4d9086e9db91df
Navid Rojiani <nrojiani@gmail.com>
2022-03-24 00:38:23 -0400

    CodeLab step 5

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -19,8 +19,11 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.BACKGROUND
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
@@ -101,11 +104,13 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      * Wait one second then update the tap count.
      */
     private fun updateTaps() {
-        // TODO: Convert updateTaps to use coroutines
-        tapCount++
-        BACKGROUND.submit {
-            Thread.sleep(1_000)
-            _taps.postValue("${tapCount} taps")
+        // launch a coroutine in viewModelScope
+        viewModelScope.launch {
+            tapCount++
+            // suspend this coroutine for one second
+            delay(1_000)
+            // resume in the main dispatcher
+            _taps.postValue("$tapCount taps")
         }
     }
 
