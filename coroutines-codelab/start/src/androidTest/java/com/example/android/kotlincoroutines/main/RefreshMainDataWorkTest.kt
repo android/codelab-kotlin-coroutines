546a07445a7ee7d818a2d8356c86d9e754ade0e5
Navid Rojiani <nrojiani@gmail.com>
2022-03-24 15:17:29 -0400

    Finish Kotlin Coroutines CodeLab

--- coroutines-codelab/start/src/androidTest/java/com/example/android/kotlincoroutines/main/RefreshMainDataWorkTest.kt
+++ coroutines-codelab/start/src/androidTest/java/com/example/android/kotlincoroutines/main/RefreshMainDataWorkTest.kt
@@ -16,15 +16,32 @@
 
 package com.example.android.kotlincoroutines.main
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.work.ListenableWorker.Result
+import androidx.work.testing.TestListenableWorkerBuilder
+import com.example.android.kotlincoroutines.fakes.MainNetworkFake
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+
 
 @RunWith(JUnit4::class)
 class RefreshMainDataWorkTest {
 
     @Test
     fun testRefreshMainDataWork() {
-        // TODO: Write this test
+        val fakeNetwork = MainNetworkFake("OK")
+
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val worker = TestListenableWorkerBuilder<RefreshMainDataWork>(context)
+            .setWorkerFactory(RefreshMainDataWork.Factory(fakeNetwork))
+            .build()
+
+        // Start the work synchronously
+        val result = worker.startWork().get()
+
+        assertThat(result).isEqualTo(Result.success())
     }
 }
--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/RefreshMainDataWork.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/RefreshMainDataWork.kt
@@ -17,12 +17,7 @@
 package com.example.android.kotlincoroutines.main
 
 import android.content.Context
-import androidx.work.CoroutineWorker
-import androidx.work.ListenableWorker
-import androidx.work.WorkManager
-import androidx.work.Worker
-import androidx.work.WorkerFactory
-import androidx.work.WorkerParameters
+import androidx.work.*
 
 /**
  * Worker job to refresh titles from the network while the app is in the background.
@@ -30,8 +25,12 @@ import androidx.work.WorkerParameters
  * WorkManager is a library used to enqueue work that is guaranteed to execute after its constraints
  * are met. It can run work even when the app is in the background, or not running.
  */
-class RefreshMainDataWork(context: Context, params: WorkerParameters, private val network: MainNetwork) :
-        CoroutineWorker(context, params) {
+class RefreshMainDataWork(
+    context: Context,
+    params: WorkerParameters,
+    private val network: MainNetwork
+) :
+    CoroutineWorker(context, params) {
 
     /**
      * Refresh the title from the network using [TitleRepository]
@@ -41,11 +40,24 @@ class RefreshMainDataWork(context: Context, params: WorkerParameters, private va
      * start just enough to run this [Worker].
      */
     override suspend fun doWork(): Result {
-        return Result.success()         // TODO: Use coroutines from WorkManager
+        val database = getDatabase(applicationContext)
+        val repository = TitleRepository(network, database.titleDao)
+
+        return try {
+            repository.refreshTitle()
+            Result.success()
+        } catch (error: TitleRefreshError) {
+            Result.failure()
+        }
+
     }
 
     class Factory(val network: MainNetwork = getNetworkService()) : WorkerFactory() {
-        override fun createWorker(appContext: Context, workerClassName: String, workerParameters: WorkerParameters): ListenableWorker? {
+        override fun createWorker(
+            appContext: Context,
+            workerClassName: String,
+            workerParameters: WorkerParameters
+        ): ListenableWorker? {
             return RefreshMainDataWork(appContext, workerParameters, network)
         }
 
